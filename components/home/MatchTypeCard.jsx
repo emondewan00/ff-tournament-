c625eb61b3f47c650ffa577a72408cda7a1d1edf
emondewan00 <dewanmohammademon@gmail.com>
2024-07-05 23:17:33 +0600

    add link match type card

--- components/home/MatchTypeCard.jsx
+++ components/home/MatchTypeCard.jsx
@@ -1,14 +1,18 @@
-const MatchTypeCard = ({color}) => {
+import Link from "next/link";
+
+const MatchTypeCard = ({ color }) => {
   return (
-    <div class="bg-red-500 p-4 rounded-lg">
-      <img
-        src="https://placehold.co/50x50"
-        alt="Classic Match"
-        class="w-12 h-12 rounded-full mb-2"
-      />
-      <h3 class="font-bold">Classic Match</h3>
-      <p>7 Matches found</p>
-    </div>
+    <Link href={"/category/cs"}>
+      <div class="bg-white/5 p-4 rounded-lg">
+        <img
+          src="https://placehold.co/50x50"
+          alt="Classic Match"
+          class="w-12 h-12 rounded-full mb-2"
+        />
+        <h3 class="font-bold">Classic Match</h3>
+        <p>7 Matches found</p>
+      </div>
+    </Link>
   );
 };
 

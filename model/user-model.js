1f32d3ecfd27c047fd8795f1a29dbce1d50718d3
emondewan00 <dewanmohammademon@gmail.com>
2024-07-21 23:53:34 +0600

    user model added

---
+++ model/user-model.js
@@ -0,0 +1,20 @@
+import { Schema, model, models } from "mongoose";
+
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  mobile: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});

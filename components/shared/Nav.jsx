e02908c81d6c10582080529dd6a67f96642cc48a
emondewan00 <dewanmohammademon@gmail.com>
2024-07-26 13:54:11 +0600

    change card bg style and nav style

--- components/shared/Nav.jsx
+++ components/shared/Nav.jsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 const Nav = () => {
   return (
-    <div className="flex items-center justify-between px-8 py-4 bg-white/20 backdrop-blur-sm shadow">
+    <div className="flex items-center justify-between px-8 py-4 bg-white/5 backdrop-blur-sm shadow-md">
       <Link href={"/"} className="flex items-center">
         <img src="https://placehold.co/40x40" alt="logo" className="mr-2" />
         <span className="font-bold text-xl">FF TOUR</span>

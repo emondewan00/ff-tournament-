c435daa6e0fff1ebf28ab01a96e4c2bce6fbf8f1
emondewan00 <dewanmohammademon@gmail.com>
2024-08-15 15:53:56 +0600

    add dynamic user field in join form

--- components/matches/JoinForm.jsx
+++ components/matches/JoinForm.jsx
@@ -2,7 +2,9 @@
 
 import { joinInMatch } from "@/actions/joinInMatch";
 import { useSession } from "next-auth/react";
+import { useFieldArray, useForm } from "react-hook-form";
 import { toast } from "react-toastify";
+import { FaMinus } from "react-icons/fa";
 
 const JoinForm = ({ onClose, id }) => {
   const {
@@ -10,17 +12,34 @@ const JoinForm = ({ onClose, id }) => {
     status,
   } = useSession();
 
-  const onSubmit = async (e) => {
-    e.preventDefault();
-    const formData = new FormData(e.target);
-    const result = await joinInMatch(formData);
-    if (result?.success) {
-      onClose();
-      toast.success(result.message);
-    } else {
-      onClose();
-      toast.error(result.message);
-    }
+  const { register, handleSubmit, control } = useForm({
+    defaultValues: {
+      players: [
+        {
+          username: 0,
+        },
+      ],
+      matchId: id,
+      userId: user?.id,
+    },
+  });
+
+  const { fields, append, remove } = useFieldArray({
+    control,
+    name: "players",
+  });
+
+  const onSubmit = async (data) => {
+    console.log(data);
+    // const formData = new FormData(e.target);
+    // const result = await joinInMatch(formData);
+    // if (result?.success) {
+    //   onClose();
+    //   toast.success(result.message);
+    // } else {
+    //   onClose();
+    //   toast.error(result.message);
+    // }
   };
 
   return (
@@ -34,16 +53,42 @@ const JoinForm = ({ onClose, id }) => {
         </div>
 
         <form
-          onSubmit={onSubmit}
+          onSubmit={handleSubmit(onSubmit)}
           className="space-y-4 w-60 p-4 bg-white rounded"
         >
-          <h1>User info </h1>
-          <input
-            type="text"
-            name="gameIdName"
-            placeholder="Enter your user name"
-            className="p-2 bg-slate-100 border border-blue-500  focus:outline focus:outline-blue-500 w-full"
-          />
+          <h1>Players User Name </h1>
+
+          {fields.map((field, index) => {
+            return (
+              <div key={field.id} className="flex">
+                <input
+                  type="text"
+                  name={field.username}
+                  placeholder="Player user name"
+                  className="p-2 bg-slate-100 border border-r-0 border-blue-500  focus:outline focus:outline-blue-500 w-full"
+                />
+
+                <button
+                  type="button"
+                  onClick={() => index !== 0 && remove(index)}
+                  className="w-10  py-2 rounded-r cursor-pointer duration-100 delay-75 transition-all ease-linear bg-red-500 text-white font-bold grid place-items-center"
+                >
+                  <FaMinus />
+                </button>
+              </div>
+            );
+          })}
+
+          {fields.length < 4 && (
+            <button
+              type="button"
+              onClick={() => append({ username: fields.length })}
+              className="w-full bg-blue-600 py-2 rounded text-white cursor-pointer hover:bg-blue-700 duration-100 delay-75 transition-all ease-linear "
+            >
+              Add Name
+            </button>
+          )}
+
           <input type="hidden" name="matchId" value={id} />
           <input type="hidden" name="userId" value={user?.id} />
           <input
